adc6be767a881bcf4cff887041e1dc6263cce743
vanminhle <{ID}+{username}@users.noreply.github.com>
2022-06-18 21:03:28 +0700

    43 - Filters - Unique Values

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11,4 +11,11 @@ export const formatPrice = (number) => {
   }).format(number / 100);
 };
 
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  console.log(unique);
+  if (type === "colors") {
+    unique = unique.flat();
+  }
+  return ["all", ...new Set(unique)];
+};
