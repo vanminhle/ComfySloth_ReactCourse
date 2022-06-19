08028953d5dc2b8b499d0ba5d8ffb574085034ed
vanminhle <{ID}+{username}@users.noreply.github.com>
2022-06-19 13:05:46 +0700

    50 - Filter Functionality - Rest of the Filters

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -80,11 +80,43 @@ const filter_reducer = (state, action) => {
     const { text, category, company, color, price, shipping } = state.filters;
 
     let tempProducts = [...all_products];
+
     //filtering
     if (text) {
       tempProducts = tempProducts.filter((product) => {
         return product.name.toLowerCase().startsWith(text);
       });
+    }
+
+    //category
+    if (category !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      );
+    }
+
+    //company
+    if (company !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      );
+    }
+
+    //color
+    if (color !== "all") {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color);
+      });
+    }
+
+    //price
+    tempProducts = tempProducts.filter((product) => product.price <= price);
+
+    //shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter(
+        (product) => product.shipping === true
+      );
     }
 
     return { ...state, filtered_products: tempProducts };
