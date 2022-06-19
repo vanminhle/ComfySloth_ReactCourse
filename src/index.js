fde8e62ea4f4efc12a729db02748de04e01306d5
vanminhle <{ID}+{username}@users.noreply.github.com>
2022-06-19 22:54:50 +0700

    67 - Auth0 - Provider

--- src/index.js
+++ src/index.js
@@ -8,13 +8,23 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+//dev-q6awaanv.au.auth0.com
+//hICQKgR5rODu9h1tuuXmvfabaQWgCA3a
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-q6awaanv.au.auth0.com"
+    clientId="hICQKgR5rODu9h1tuuXmvfabaQWgCA3a"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
