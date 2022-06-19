4d801cf6d66a386dcc97bfb1a29693f5a6089d75
vanminhle <{ID}+{username}@users.noreply.github.com>
2022-06-19 17:19:36 +0700

    56 - LocalStorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,18 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+//check local storage
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -33,6 +43,11 @@ export const CartProvider = ({ children }) => {
 
   //clear cart
   const clearCart = () => {};
+
+  //local storage
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <CartContext.Provider
